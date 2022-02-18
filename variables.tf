f0e65f8a20ba6985a7dc666c977b0bed1dccfc27
levente-simon <ls@leventesimon.com>
2022-02-18 20:14:22 +0100

    k8s token auth

--- variables.tf
+++ variables.tf
@@ -1,8 +1,32 @@
-variable "k8s_host"                   { type = string }
-variable "k8s_client_certificate"     { type = string }
-variable "k8s_client_key"             { type = string }
-variable "k8s_cluster_ca_certificate" { type = string }
-
 variable "namespace"                  { type = string }
 variable "etcd_fqdn"                  { type = string }
 variable "rootpwd"                    { type = string }
+
+variable "k8s_host" {
+  type      = string
+  sensitive = true
+}
+
+variable "k8s_client_certificate" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
+
+variable "k8s_client_key" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
+
+variable "k8s_cluster_ca_certificate" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
+
+variable "k8s_cluster_client_token" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
